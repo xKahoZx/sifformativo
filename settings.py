8d5fe4bd2e7207161db6382764945bfe04583aa8
xKahoZx <alejandro-177-@hotmail.com>
2015-10-25 12:45:18 -0500

    deploy

--- settings.py
+++ settings.py
@@ -12,20 +12,20 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-		'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-		'NAME': 'sif',                      # Or path to database file if using sqlite3.
+		'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+		'NAME': 'base_sif.sqlite3',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
+        'USER': '',
         'PASSWORD': '',
-		'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '3306',                      # Set to empty string for default.
+		'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
 
     }
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['demodaniel.herokuapp.com']
+ALLOWED_HOSTS = ['sifformativo.herokuapp.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -63,7 +63,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ""
+STATIC_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__),'static/'))
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -71,7 +71,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(os.path.dirname(__file__), 'static'),
+    
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -131,8 +131,8 @@ INSTALLED_APPS = (
     'sif.apps.home',
     'sif.apps.inventario',
     'sif.apps.web_services.ws_producto',
-    #'rest_framework',
-    #'gunicorn',    
+    'rest_framework',
+    'gunicorn',    
 
 )
 
