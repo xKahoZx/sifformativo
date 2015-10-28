d57e8337cf941b7a1444a448cd8d23049597e2e1
Esteban <esteban22x@gmail.com>
2015-10-28 14:35:34 -0500

    arreglo todo el base

--- sif/media/js/principal.js
+++ sif/media/js/principal.js
@@ -11,4 +11,17 @@ $(function(){
 	$("#id_codigobarras").scannerDetection({stopPropagation:true,preventDefault:true},function(datos){
 		$("#id_cantidad").focus();
 	});
+	$('.datepicker').pickadate({
+    	selectMonths: true, // Creates a dropdown to control month
+    	selectYears: 15 // Creates a dropdown of 15 years to control year
+  	});
+  	 $('.parallax').parallax();
+  	 //$('select').material_select();
+	$('select').material_select();
+	$(".button-collapse").sideNav();
+	$('.modal-trigger').leanModal();
+	$('.datepicker').pickadate({
+		selectMonths: true, // Creates a dropdown to control month
+		selectYears: 15 // Creates a dropdown of 15 years to control year
+	});
 });
